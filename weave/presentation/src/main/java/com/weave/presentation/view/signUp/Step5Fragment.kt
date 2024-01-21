374dceeccb82807c47ee7e3f870b42a7fc8221e5
moondev03 <moondev03@gmail.com>
2024-01-21 23:08:24 +0900

    [WEAV-40] :lipstick: STEP5 - 학과

--- weave/presentation/src/main/java/com/weave/presentation/view/signUp/Step5Fragment.kt
+++ weave/presentation/src/main/java/com/weave/presentation/view/signUp/Step5Fragment.kt
@@ -8,22 +8,19 @@ import android.view.inputmethod.InputMethodManager
 import androidx.fragment.app.activityViewModels
 import com.weave.presentation.R
 import com.weave.presentation.base.BaseFragment
-import com.weave.presentation.databinding.FragmentSignUpStep4Binding
+import com.weave.presentation.databinding.FragmentSignUpStep5Binding
 import com.weave.presentation.utils.CustomAutoCompleteViewAdapter
 import com.weave.presentation.viewmodel.SignUpViewModel
 
-class Step4Fragment: BaseFragment<FragmentSignUpStep4Binding>(R.layout.fragment_sign_up_step_4) {
+class Step5Fragment: BaseFragment<FragmentSignUpStep5Binding>(R.layout.fragment_sign_up_step_5) {
     private val viewModel: SignUpViewModel by activityViewModels()
 
     override fun init() {
         binding.vm = viewModel
         binding.lifecycleOwner = viewLifecycleOwner
 
-        if(viewModel.currentUniv.value != ""){
-            viewModel.setNextBtn(true)
-        } else {
-            viewModel.setNextBtn(false)
-        }
+        viewModel.setNextBtn(false)
+        viewModel.setIsEmpty(true)
 
         binding.appBar.ibAppBarSignUpBack.setOnClickListener {
             viewModel.setNextBtn(true)
@@ -32,28 +29,23 @@ class Step4Fragment: BaseFragment<FragmentSignUpStep4Binding>(R.layout.fragment_
         binding.appBar.ibAppBarSignUpCancel.setOnClickListener {  }
 
         binding.ibNext.setOnClickListener {
-            if(viewModel.nextBtn.value!!){
-                requireActivity().supportFragmentManager.beginTransaction()
-                    .replace(R.id.fl_sign_up, Step5Fragment())
-                    .addToBackStack(null)
-                    .commit()
-            }
+            viewModel.printResult()
         }
 
-         binding.clStep4.setOnClickListener {
+         binding.clStep5.setOnClickListener {
              binding.etAuto.clearFocus()
              val inputMethodManager = requireContext().getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
              inputMethodManager.hideSoftInputFromWindow(view?.windowToken, 0)
          }
 
-        val autoAdapter = CustomAutoCompleteViewAdapter(requireContext(), R.layout.item_dropdown_univ, autoData)
+        val autoAdapter = CustomAutoCompleteViewAdapter(requireContext(), R.layout.item_dropdown_major, autoData)
 
         binding.etAuto.setDropDownBackgroundResource(R.drawable.shape_dropdown_layout)
         binding.etAuto.setAdapter(autoAdapter)
 
         binding.etAuto.setOnItemClickListener { parent, _, position, _ ->
             val selectedItem = parent.getItemAtPosition(position) as String
-            viewModel.setUniv(selectedItem)
+            viewModel.setMajor(selectedItem)
         }
 
 
@@ -62,21 +54,13 @@ class Step4Fragment: BaseFragment<FragmentSignUpStep4Binding>(R.layout.fragment_
             override fun afterTextChanged(s: Editable?) {}
 
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                // clear btn 활성화 체크
                 if(s.toString().isNotEmpty()){
                     viewModel.setIsEmpty(false)
                 } else {
                     viewModel.setIsEmpty(true)
                 }
+                viewModel.setNextBtn(false)
 
-                // BackStack으로 돌아왔을 때 체크
-                if(s.toString() == viewModel.currentUniv.value){
-                    viewModel.setNextBtn(true)
-                } else {
-                    viewModel.setNextBtn(false)
-                }
-
-                // 검색 결과 사이즈에 따른 DropDown 크기 지정
                 if(binding.etAuto.adapter.count >= 3){
                     binding.etAuto.dropDownHeight = 56*4*3-48
                 } else {
@@ -93,29 +77,24 @@ class Step4Fragment: BaseFragment<FragmentSignUpStep4Binding>(R.layout.fragment_
     }
 
     val autoData = listOf(
-        "서울대학교",
-        "연세대학교",
-        "고려대학교",
-        "한양대학교",
-        "성균관대학교",
-        "서강대학교",
-        "중앙대학교",
-        "경희대학교",
-        "한국과학기술원",
-        "POSTECH",
-        "명지대학교",
-        "서울시립대학교",
-        "성신여자대학교",
-        "이화여자대학교",
-        "숙명여자대학교",
-        "동국대학교",
-        "부산대학교",
-        "경북대학교",
-        "전남대학교",
-        "충남대학교",
-        "강원1대학교",
-        "강원2대학교",
-        "강원3대학교",
-        "강원4대학교"
+        "컴퓨터 공학",
+        "전자 공학",
+        "화학 공학",
+        "경제학",
+        "사회학",
+        "의학",
+        "간호학",
+        "물리학",
+        "수학",
+        "영어 학문",
+        "국어 학문",
+        "음악학",
+        "미술사",
+        "역사학",
+        "심리학",
+        "경영학",
+        "법학",
+        "정치학",
+        "환경 공학"
     )
 }
