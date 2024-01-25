220e9207f0a3bac76ee579c0a76b849f5a64f5c1
moondev03 <moondev03@gmail.com>
2024-01-25 23:38:38 +0900

    :sparkles: [WEAV-41] DataStore 세팅

--- Weave/app/src/main/java/com/weave/weave/core/GlobalApplication.kt
+++ Weave/app/src/main/java/com/weave/weave/core/GlobalApplication.kt
@@ -5,8 +5,14 @@ import android.util.Log
 import com.kakao.sdk.common.KakaoSdk
 import com.kakao.sdk.common.util.Utility
 import com.weave.weave.BuildConfig
+import com.weave.weave.data.local.DataStoreModule
 
 class GlobalApplication: Application() {
+    private lateinit var dataStore: DataStoreModule
+    companion object {
+        lateinit var app: GlobalApplication
+    }
+
     override fun onCreate() {
         super.onCreate()
 
@@ -14,5 +20,10 @@ class GlobalApplication: Application() {
 
         val keyHash = Utility.getKeyHash(this)
         Log.i("Hash", keyHash)
+
+        app = this
+        dataStore = DataStoreModule(this)
     }
+
+    fun getDataStore(): DataStoreModule = dataStore
 }
---
+++ Weave/app/src/main/java/com/weave/weave/data/local/DataStoreModule.kt
@@ -0,0 +1,55 @@
+package com.weave.weave.data.local
+
+import android.content.Context
+import android.util.Log
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.emptyPreferences
+import androidx.datastore.preferences.core.stringPreferencesKey
+import androidx.datastore.preferences.preferencesDataStore
+import com.weave.weave.data.local.DataStoreModule.PreferencesKeys.ACCESS_TOKEN
+import com.weave.weave.data.local.DataStoreModule.PreferencesKeys.REFRESH_TOKEN
+import com.weave.weave.data.local.dto.UserPreferences
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.map
+import java.io.IOException
+
+class DataStoreModule(private val context: Context){
+    private val Context.dataStore by preferencesDataStore(name = "user_prefs")
+
+    private object PreferencesKeys {
+        val ACCESS_TOKEN = stringPreferencesKey("access_token")
+        val REFRESH_TOKEN = stringPreferencesKey("refresh_token")
+    }
+
+    suspend fun getLoginToken(): Flow<UserPreferences> = context.dataStore.data
+        .catch { exception ->
+            if(exception is IOException){
+                Log.e("Error reading preferences.", exception.toString())
+                emit(emptyPreferences())
+            } else {
+                throw exception
+            }
+        }.map { preferences ->
+            mapUserPreferences(preferences)
+        }
+
+    suspend fun updatePreferencesRefreshToken(refreshToken: String){
+        context.dataStore.edit { preferences ->
+            preferences[REFRESH_TOKEN] = refreshToken
+        }
+    }
+
+    suspend fun updatePreferencesAccessToken(accessToken: String){
+        context.dataStore.edit { preferences ->
+            preferences[ACCESS_TOKEN] = accessToken
+        }
+    }
+
+    private fun mapUserPreferences(preferences: Preferences): UserPreferences {
+        val accessToken = preferences[ACCESS_TOKEN] ?: "NULL"
+        val refreshToken = preferences[REFRESH_TOKEN] ?: "NULL"
+        return UserPreferences(accessToken, refreshToken)
+    }
+}
