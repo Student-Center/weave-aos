d5df46238c8b8918e6a7a8f713dd93c746a5c79c
MunJangHun <105299421+moondev03@users.noreply.github.com>
2024-01-27 01:21:00 +0900

    [WEAV-67] 바텀 네비게이션 추가 (#7)
    * [WEAV-67] :bento: 바텀 네비 svg 추가
    
    * [WEAV-67] :lipstick: 바텀 네비 style 작성
    
    * [WEAV-67] :heavy_plus_sign: Glide 의존성 추가
    
    * [WEAV-67] :sparkles: 바텀 네비게이션 추가
    
    * [WEAV-67] :lipstick: color.xml 수정

---
+++ Weave/app/src/main/java/com/weave/weave/presentation/view/chat/ChatFragment.kt
@@ -0,0 +1,6 @@
+package com.weave.weave.presentation.view.chat
+
+import androidx.fragment.app.Fragment
+
+class ChatFragment: Fragment() {
+}
---
+++ Weave/app/src/main/java/com/weave/weave/presentation/view/home/HomeFragment.kt
@@ -0,0 +1,6 @@
+package com.weave.weave.presentation.view.home
+
+import androidx.fragment.app.Fragment
+
+class HomeFragment: Fragment() {
+}
