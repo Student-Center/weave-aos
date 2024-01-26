c834b98ebca991aafc2b59a05babef185f757505
moondev03 <moondev03@gmail.com>
2024-01-27 01:18:08 +0900

    [WEAV-67] :sparkles: 바텀 네비게이션 추가

---
+++ Weave/app/src/main/java/com/weave/weave/presentation/view/my/MyFragment.kt
@@ -0,0 +1,6 @@
+package com.weave.weave.presentation.view.my
+
+import androidx.fragment.app.Fragment
+
+class MyFragment: Fragment() {
+}
---
+++ Weave/app/src/main/java/com/weave/weave/presentation/view/team/TeamFragment.kt
@@ -0,0 +1,6 @@
+package com.weave.weave.presentation.view.team
+
+import androidx.fragment.app.Fragment
+
+class TeamFragment: Fragment() {
+}
