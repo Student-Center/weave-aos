5cbe2253f49d80be79505f52d488376b49ed18bb
moondev03 <moondev03@gmail.com>
2024-01-31 19:57:38 +0900

    [WEAV-126] :lipstick: 홈 레이아웃 작업

--- Weave/app/src/main/java/com/weave/weave/presentation/view/home/HomeFragment.kt
+++ Weave/app/src/main/java/com/weave/weave/presentation/view/home/HomeFragment.kt
@@ -1,6 +1,28 @@
 package com.weave.weave.presentation.view.home
 
-import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.weave.presentation.base.BaseFragment
+import com.weave.weave.R
+import com.weave.weave.databinding.FragmentHomeBinding
+import com.weave.weave.domain.entity.home.TeamTestEntity
 
-class HomeFragment: Fragment() {
+class HomeFragment: BaseFragment<FragmentHomeBinding>(R.layout.fragment_home) {
+    private lateinit var adapter: HomeRvAdapter
+    private val data=mutableListOf<TeamTestEntity>()
+
+    override fun init() {
+        initializeList()
+        initRecyclerView()
+    }
+
+    private fun initRecyclerView(){
+        adapter = HomeRvAdapter()
+        adapter.dataList = data
+        binding.rvHome.adapter = adapter
+        binding.rvHome.layoutManager = LinearLayoutManager(requireContext())
+    }
+
+    private fun initializeList(){
+
+    }
 }
