c18e68f4a16e83a175f019f166129bb95ff452c3
moondev03 <moondev03@gmail.com>
2024-02-01 16:54:19 +0900

    [WEAV-139] Main Tab에 뒤로가기 리스너 추가

--- Weave/app/src/main/java/com/weave/weave/presentation/view/my/MyFragment.kt
+++ Weave/app/src/main/java/com/weave/weave/presentation/view/my/MyFragment.kt
@@ -1,13 +1,29 @@
 package com.weave.weave.presentation.view.my
 
+import android.widget.Toast
+import androidx.activity.OnBackPressedCallback
 import com.weave.presentation.base.BaseFragment
 import com.weave.weave.R
 import com.weave.weave.databinding.FragmentMyPageBinding
 import com.weave.weave.presentation.view.MainActivity
 
 class MyFragment: BaseFragment<FragmentMyPageBinding>(R.layout.fragment_my_page) {
+    private var backPressedTime: Long = 0L
+
+    private val callback = object : OnBackPressedCallback(true) {
+        override fun handleOnBackPressed() {
+            if (System.currentTimeMillis() - backPressedTime <= 2000) {
+                requireActivity().finishAffinity()
+            } else {
+                backPressedTime = System.currentTimeMillis()
+                Toast.makeText(requireContext(), "한 번 더 누르면 종료됩니다.", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
     override fun init() {
         (requireActivity() as MainActivity).setNaviVisible(true)
+        requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner, callback)
 
         binding.ibSetting.setOnClickListener {
             requireActivity().supportFragmentManager.beginTransaction()
