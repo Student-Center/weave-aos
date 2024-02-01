52b82bbcecb8726e6a96cd56af145e04ce37b303
moondev03 <moondev03@gmail.com>
2024-02-01 18:31:42 +0900

    [WEAV-133] MBTI 정보 Edit 레이아웃

--- Weave/app/src/main/java/com/weave/weave/presentation/view/my/MyFragment.kt
+++ Weave/app/src/main/java/com/weave/weave/presentation/view/my/MyFragment.kt
@@ -1,13 +1,18 @@
 package com.weave.weave.presentation.view.my
 
+import MbtiEditBottomSheetDialog
 import android.widget.Toast
 import androidx.activity.OnBackPressedCallback
+import androidx.fragment.app.viewModels
 import com.weave.presentation.base.BaseFragment
 import com.weave.weave.R
 import com.weave.weave.databinding.FragmentMyPageBinding
 import com.weave.weave.presentation.view.MainActivity
+import com.weave.weave.presentation.viewmodel.MyViewModel
 
 class MyFragment: BaseFragment<FragmentMyPageBinding>(R.layout.fragment_my_page) {
+    private val viewModel: MyViewModel by viewModels()
+
     private var backPressedTime: Long = 0L
 
     private val callback = object : OnBackPressedCallback(true) {
@@ -25,11 +30,28 @@ class MyFragment: BaseFragment<FragmentMyPageBinding>(R.layout.fragment_my_page)
         (requireActivity() as MainActivity).setNaviVisible(true)
         requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner, callback)
 
+        binding.lifecycleOwner = viewLifecycleOwner
+        binding.vm = viewModel
+
+        initInfo()
+
         binding.ibSetting.setOnClickListener {
             requireActivity().supportFragmentManager.beginTransaction()
                 .replace(R.id.fl_main, SettingFragment())
                 .addToBackStack(null)
                 .commit()
         }
+
+        binding.tvMbtiBtn.setOnClickListener {
+            MbtiEditBottomSheetDialog.getInstance(viewModel).show(requireActivity().supportFragmentManager, "mbti")
+        }
+    }
+
+    // test
+    private fun initInfo(){
+        viewModel.setLineValue(1, "I")
+        viewModel.setLineValue(2, "S")
+        viewModel.setLineValue(3, "F")
+        viewModel.setLineValue(4, "J")
     }
 }
---
+++ Weave/app/src/main/java/com/weave/weave/presentation/viewmodel/MyViewModel.kt
@@ -0,0 +1,55 @@
+package com.weave.weave.presentation.viewmodel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class MyViewModel: ViewModel() {
+    private var _saveBtn = MutableLiveData(false)
+    val nextBtn: LiveData<Boolean>
+        get() = _saveBtn
+
+    fun setSaveBtn(p: Boolean){
+        _saveBtn.value = p
+    }
+
+    // UserInfo
+    private var _mbti = MutableLiveData("ISFJ")
+    val mbti: LiveData<String>
+        get() = _mbti
+
+    fun setMbti(){
+        _mbti.value = "${line1.value}${line2.value}${line3.value}${line4.value}"
+    }
+
+
+    // Edit-MBTI
+    private var _line1 = MutableLiveData("")
+    val line1: LiveData<String>
+        get() = _line1
+
+    private var _line2 = MutableLiveData("")
+    val line2: LiveData<String>
+        get() = _line2
+
+    private var _line3 = MutableLiveData("")
+    val line3: LiveData<String>
+        get() = _line3
+
+    private var _line4 = MutableLiveData("")
+    val line4: LiveData<String>
+        get() = _line4
+
+    fun setLineValue(line: Int, p: String){
+        when(line){
+            1 -> _line1.value = p
+            2 -> _line2.value = p
+            3 -> _line3.value = p
+            4 -> _line4.value = p
+        }
+
+        if(line1.value != "" && line2.value != "" && line3.value != "" && line4.value != ""){
+            _saveBtn.value = true
+        }
+    }
+}
