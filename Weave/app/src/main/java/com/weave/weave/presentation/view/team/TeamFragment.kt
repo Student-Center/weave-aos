c4741f321b75ad638b2d2b5c0f4c8fd8586f434e
moondev03 <moondev03@gmail.com>
2024-02-03 00:06:07 +0900

    [WEAV-150] 팀 생성 페이지 레이아웃

--- Weave/app/src/main/java/com/weave/weave/presentation/view/team/TeamFragment.kt
+++ Weave/app/src/main/java/com/weave/weave/presentation/view/team/TeamFragment.kt
@@ -1,11 +1,35 @@
 package com.weave.weave.presentation.view.team
 
+import android.widget.Toast
+import androidx.activity.OnBackPressedCallback
 import com.weave.weave.R
 import com.weave.weave.databinding.FragmentTeamBinding
 import com.weave.weave.presentation.base.BaseFragment
+import com.weave.weave.presentation.view.MainActivity
 
 class TeamFragment: BaseFragment<FragmentTeamBinding>(R.layout.fragment_team) {
-    override fun init() {
+    private var backPressedTime: Long = 0L
 
+    private val callback = object : OnBackPressedCallback(true) {
+        override fun handleOnBackPressed() {
+            if (System.currentTimeMillis() - backPressedTime <= 2000) {
+                requireActivity().finishAffinity()
+            } else {
+                backPressedTime = System.currentTimeMillis()
+                Toast.makeText(requireContext(), "한 번 더 누르면 종료됩니다.", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    override fun init() {
+        (requireActivity() as MainActivity).setNaviVisible(true)
+        requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner, callback)
+
+        binding.btnNew.setOnClickListener {
+            requireActivity().supportFragmentManager.beginTransaction()
+                .replace(R.id.fl_main, TeamNewFragment())
+                .addToBackStack(null)
+                .commit()
+        }
     }
 }
