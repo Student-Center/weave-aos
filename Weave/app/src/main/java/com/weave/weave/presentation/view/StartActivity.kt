2d1b2b15ffce2480e1c18d44e2e14b7e0450227b
moondev03 <moondev03@gmail.com>
2024-02-07 14:43:00 +0900

    [WEAV-86] 토큰 유효성 검사 로직 변경

--- Weave/app/src/main/java/com/weave/weave/presentation/view/StartActivity.kt
+++ Weave/app/src/main/java/com/weave/weave/presentation/view/StartActivity.kt
@@ -11,6 +11,7 @@ import com.weave.weave.core.GlobalApplication.Companion.app
 import com.weave.weave.data.remote.dto.auth.RefreshTokenReq
 import com.weave.weave.domain.usecase.RefreshLoginTokenUseCase
 import com.weave.weave.domain.usecase.Resource
+import com.weave.weave.domain.usecase.profile.GetMyInfoUseCase
 import com.weave.weave.presentation.view.signIn.SignInActivity
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -32,8 +33,8 @@ class StartActivity: AppCompatActivity() {
         splashScreen.setOnExitAnimationListener { splashScreenView ->
             Thread {
                 runOnUiThread {
-//                    serverTokenValid()
-                    moveActivity(MainActivity())
+                    serverTokenValid()
+//                    moveActivity(MainActivity())
                     splashScreenView.remove()
                 }
             }.start()
@@ -52,15 +53,17 @@ class StartActivity: AppCompatActivity() {
                     }
                 }
 
-                // AccessToken 유효
-                // MainActivity로 이동
-//                if(){
-//                  moveActivity(MainActivity())
-//                }
+                tokenValidation()
+            }
+        }
+    }
 
+    private fun refreshLoginToken(){
+        CoroutineScope(Dispatchers.IO).launch {
+            app.getUserDataStore().getLoginToken().collect {
                 // 재발급 Interceptor에서 loginState가 true일 때만 수행하도록 해야 함
                 Log.i("START", "토큰 재발급 진행")
-                when(val res = RefreshLoginTokenUseCase().refreshLoginToken(RefreshTokenReq(refreshToken))){
+                when(val res = RefreshLoginTokenUseCase().refreshLoginToken(RefreshTokenReq(it.refreshToken))){
                     // AccessToken 만료, RefreshToken 유효
                     // 토큰 재발급 성공
                     is Resource.Success -> {
@@ -68,13 +71,8 @@ class StartActivity: AppCompatActivity() {
                         app.getUserDataStore().updatePreferencesRefreshToken(res.data.refreshToken)
                         app.getUserDataStore().updatePreferencesAccessToken(res.data.accessToken)
 
-                        // 재발급한 AccessToken으로 유효성 검사 다시 진행 후 MainActivity로 이동
-//                             if(){
-//                                 launch(Dispatchers.Main){
-//                                     moveActivity(MainActivity())
-//                                 }
-//                             }
-
+//                      재발급한 AccessToken으로 유효성 검사 다시 진행 후 MainActivity로 이동
+                        tokenValidation()
                     }
                     // AccessToken 만료, RefreshToken 만료 (재발급 실패 경우)
                     // 로컬에 저장된 유저 데이터 지우고 SignInActivity로 이동
@@ -96,5 +94,24 @@ class StartActivity: AppCompatActivity() {
     private fun moveActivity(p: Any){
         val intent = Intent(this, p::class.java)
         startActivity(intent)
+        finish()
+    }
+
+    private suspend fun tokenValidation(){
+        app.getUserDataStore().getLoginToken().collect {
+            when(val res = GetMyInfoUseCase().getMyInfo(it.accessToken)){
+                is Resource.Success -> {
+                    Log.i("START", "토큰 유효 -> 메인 액티비티 이동")
+                    CoroutineScope(Dispatchers.Main).launch{
+                        moveActivity(MainActivity())
+                    }
+                }
+                is Resource.Error -> {
+                    Log.e("START", "토큰 유효하지 않음: ${res.message}")
+                    refreshLoginToken()
+                }
+                else -> {}
+            }
+        }
     }
 }
