751e1605b3d2353ed56ba43f09d6841d3771f3d0
moondev03 <moondev03@gmail.com>
2024-02-10 00:44:55 +0900

    [WEAV-149] 요청 - Empty 시 홈으로 이동 가능한 버튼 추가

--- Weave/app/src/main/java/com/weave/weave/presentation/view/request/RequestReceivedFragment.kt
+++ Weave/app/src/main/java/com/weave/weave/presentation/view/request/RequestReceivedFragment.kt
@@ -1,11 +1,16 @@
 package com.weave.weave.presentation.view.request
 
+import androidx.core.view.get
 import com.weave.weave.R
 import com.weave.weave.databinding.FragmentRequestReceivedBinding
 import com.weave.weave.presentation.base.BaseFragment
+import com.weave.weave.presentation.view.MainActivity
 
 class RequestReceivedFragment: BaseFragment<FragmentRequestReceivedBinding>(R.layout.fragment_request_received) {
     override fun init() {
-
+        binding.btnMove.setOnClickListener {
+            val mainActivity = (requireContext() as MainActivity)
+            mainActivity.binding.bottomNavi.selectedItemId = mainActivity.binding.bottomNavi.menu[2].itemId
+        }
     }
 }
