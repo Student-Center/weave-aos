76102ee5328d7dffb2b45d1230a030f7f8b09c0d
moondev03 <moondev03@gmail.com>
2024-02-29 01:43:30 +0900

    [WEAV-209] 테스트 데이터 (홈, 요청)

--- Weave/app/src/main/java/com/studentcenter/weave/presentation/view/request/RequestReceivedFragment.kt
+++ Weave/app/src/main/java/com/studentcenter/weave/presentation/view/request/RequestReceivedFragment.kt
@@ -5,6 +5,10 @@ import androidx.core.view.get
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.studentcenter.weave.R
 import com.studentcenter.weave.databinding.FragmentRequestReceivedBinding
+import com.studentcenter.weave.domain.entity.meeting.MeetingListItemEntity
+import com.studentcenter.weave.domain.entity.meeting.MeetingListMemberInfoEntity
+import com.studentcenter.weave.domain.entity.meeting.MeetingListTeamEntity
+import com.studentcenter.weave.domain.enums.TeamType
 import com.studentcenter.weave.presentation.base.BaseFragment
 import com.studentcenter.weave.presentation.view.MainActivity
 import com.studentcenter.weave.presentation.viewmodel.RequestViewModel
@@ -29,6 +33,126 @@ class RequestReceivedFragment(private val vm: RequestViewModel): BaseFragment<Fr
             adapter.changeList(vm.receiveData.value!!)
             if(initFlag) { rvVisibility() }
             else { initFlag = true }
+
+            if(it.isNullOrEmpty()){
+                binding.llEmpty.visibility = View.GONE
+                binding.rvRequestReceived.visibility = View.VISIBLE
+
+                adapter.changeList(
+                    listOf(
+                        MeetingListItemEntity(
+                            id = "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+                            requestingTeam = MeetingListTeamEntity(
+                                id = "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+                                teamIntroduce = "테스트",
+                                memberCount = 2,
+                                gender = "string",
+                                memberInfos = listOf(
+                                    MeetingListMemberInfoEntity(
+                                        id = "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+                                        userId = "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+                                        universityName = "위브대",
+                                        mbti = "ISFJ",
+                                        birthYear = 2000,
+                                        animalType = "CAT"
+                                    ),
+                                    MeetingListMemberInfoEntity(
+                                        id = "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+                                        userId = "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+                                        universityName = "위브대",
+                                        mbti = "ENTJ",
+                                        birthYear = 2001,
+                                        animalType = "DOG"
+                                    )
+                                )
+                            ),
+                            receivingTeam = MeetingListTeamEntity(
+                                id = "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+                                teamIntroduce = "string",
+                                memberCount = 0,
+                                gender = "string",
+                                memberInfos = listOf(
+                                    MeetingListMemberInfoEntity(
+                                        id = "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+                                        userId = "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+                                        universityName = "위브대",
+                                        mbti = "ENTJ",
+                                        birthYear = 2001,
+                                        animalType = "DOG"
+                                    ),
+                                    MeetingListMemberInfoEntity(
+                                        id = "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+                                        userId = "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+                                        universityName = "위브대",
+                                        mbti = "INTJ",
+                                        birthYear = 2002,
+                                        animalType = "CAT"
+                                    )
+                                )
+                            ),
+                            teamType = TeamType.RECEIVING.toString(),
+                            status = "PENDING",
+                            createdAt = "2024-02-28T16:06:16.729Z",
+                            pendingEndAt = "2024-02-28T16:06:16.729Z"
+                        ),
+                        MeetingListItemEntity(
+                            id = "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+                            requestingTeam = MeetingListTeamEntity(
+                                id = "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+                                teamIntroduce = "string",
+                                memberCount = 2,
+                                gender = "string",
+                                memberInfos = listOf(
+                                    MeetingListMemberInfoEntity(
+                                        id = "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+                                        userId = "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+                                        universityName = "위브대",
+                                        mbti = "ISTP",
+                                        birthYear = 2001,
+                                        animalType = "DOG"
+                                    ),
+                                    MeetingListMemberInfoEntity(
+                                        id = "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+                                        userId = "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+                                        universityName = "위브대",
+                                        mbti = "INTJ",
+                                        birthYear = 1998,
+                                        animalType = "CAT"
+                                    )
+                                )
+                            ),
+                            receivingTeam = MeetingListTeamEntity(
+                                id = "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+                                teamIntroduce = "string",
+                                memberCount = 2,
+                                gender = "string",
+                                memberInfos = listOf(
+                                    MeetingListMemberInfoEntity(
+                                        id = "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+                                        userId = "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+                                        universityName = "위브대",
+                                        mbti = "ISTP",
+                                        birthYear = 2001,
+                                        animalType = "DOG"
+                                    ),
+                                    MeetingListMemberInfoEntity(
+                                        id = "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+                                        userId = "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+                                        universityName = "위브대",
+                                        mbti = "INTJ",
+                                        birthYear = 1998,
+                                        animalType = "CAT"
+                                    )
+                                )
+                            ),
+                            teamType = TeamType.RECEIVING.toString(),
+                            status = "PENDING",
+                            createdAt = "2024-02-28T16:06:16.729Z",
+                            pendingEndAt = "2024-02-28T16:06:16.729Z"
+                        )
+                    )
+                )
+            }
         }
     }
 
