9113d52681e8c033e1fb7d391b0037d3155844a3
moondev03 <moondev03@gmail.com>
2024-03-02 23:47:48 +0900

    [FIX] TimeUtil Formatter 문제 수정

--- Weave/app/src/main/java/com/studentcenter/weave/presentation/util/TimeUtil.kt
+++ Weave/app/src/main/java/com/studentcenter/weave/presentation/util/TimeUtil.kt
@@ -1,16 +1,31 @@
 package com.studentcenter.weave.presentation.util
 
+import android.util.Log
 import java.time.Duration
 import java.time.LocalDateTime
 import java.time.ZoneId
-import java.time.ZonedDateTime
+import java.time.format.DateTimeFormatter
+import java.time.format.DateTimeParseException
 
 class TimeUtil {
     fun getRemainingTimeMessage(endTimeString: String): String {
         val currentTime = LocalDateTime.now(ZoneId.of("Asia/Seoul"))
-        val endTime = ZonedDateTime.parse(endTimeString)
+        var parsedDateTime: LocalDateTime? = null
 
-        val remainingTime = Duration.between(currentTime, endTime.toLocalDateTime())
+        try {
+            parsedDateTime = LocalDateTime.parse(endTimeString, DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'"))
+        } catch (e: DateTimeParseException) {
+            try {
+                parsedDateTime = LocalDateTime.parse(endTimeString, DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSSSS"))
+            } catch (e: DateTimeParseException) {
+                Log.i("TimeUtil", "날짜 형식이 유효하지 않습니다.")
+            }
+        }
+
+        Log.i("TEST", "$currentTime / $parsedDateTime")
+
+
+        val remainingTime = Duration.between(currentTime, parsedDateTime)
 
         val hours = remainingTime.toHours()
         val minutes = remainingTime.toMinutes() % 60
