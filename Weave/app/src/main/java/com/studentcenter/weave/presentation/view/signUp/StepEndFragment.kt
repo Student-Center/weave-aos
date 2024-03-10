4df541f66cb939562030e16addd1588a8c694402
moondev03 <moondev03@gmail.com>
2024-03-10 19:01:57 +0900

    [WEAV-289] 약관 동의 페이지 추가

---
+++ Weave/app/src/main/java/com/studentcenter/weave/presentation/view/signUp/StepEndFragment.kt
@@ -0,0 +1,127 @@
+package com.studentcenter.weave.presentation.view.signUp
+
+import android.content.Intent
+import android.net.Uri
+import android.util.Log
+import android.widget.CompoundButton
+import android.widget.Toast
+import androidx.fragment.app.activityViewModels
+import com.studentcenter.weave.BuildConfig
+import com.studentcenter.weave.R
+import com.studentcenter.weave.core.GlobalApplication
+import com.studentcenter.weave.core.GlobalApplication.Companion.registerToken
+import com.studentcenter.weave.databinding.FragmentSignUpStepEndBinding
+import com.studentcenter.weave.domain.usecase.Resource
+import com.studentcenter.weave.domain.usecase.auth.RegisterUserUseCase
+import com.studentcenter.weave.presentation.base.BaseFragment
+import com.studentcenter.weave.presentation.util.CustomDialog
+import com.studentcenter.weave.presentation.view.MainActivity
+import com.studentcenter.weave.presentation.viewmodel.SignUpViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.runBlocking
+
+class StepEndFragment: BaseFragment<FragmentSignUpStepEndBinding>(R.layout.fragment_sign_up_step_end) {
+    private val viewModel: SignUpViewModel by activityViewModels()
+
+    override fun init() {
+        viewModel.setNextBtn(false)
+        viewModel.setIsEmpty(true)
+
+        binding.appBar.ibAppBarSignUpCancel.setOnClickListener {
+            CustomDialog.getInstance(CustomDialog.DialogType.SIGN_UP_CANCEL, null).show(requireActivity().supportFragmentManager, "cancelDialog")
+        }
+        binding.appBar.ibAppBarSignUpBack.setOnClickListener {
+            viewModel.setMajor("")
+            requireActivity().supportFragmentManager.popBackStack()
+        }
+
+        binding.btnNext.setOnClickListener {
+            if(binding.cbTosAll.isChecked){
+                if(registerUser()){
+                    GlobalApplication.loginState = true
+                    val intent = Intent(requireContext(), MainActivity::class.java)
+                    intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                    startActivity(intent)
+
+                } else {
+                    Toast.makeText(requireContext(), "재접속 후 다시 시도해주세요.", Toast.LENGTH_SHORT).show()
+                }
+            } else {
+                Toast.makeText(requireContext(), "필수 동의 항목을 체크 해주세요", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        with(binding){
+            cbTosAll.setOnCheckedChangeListener { it, _ -> onCheckChanged(it) }
+            cbTosAge.setOnCheckedChangeListener { it, _ -> onCheckChanged(it) }
+            cbTosPrivacy.setOnCheckedChangeListener { it, _ -> onCheckChanged(it) }
+            cbTosTos.setOnCheckedChangeListener { it, _ -> onCheckChanged(it) }
+        }
+
+        binding.tvLinkTos.setOnClickListener { openInternet(BuildConfig.TOS_URL) }
+        binding.tvLinkPrivacy.setOnClickListener { openInternet(BuildConfig.PRIVACY_POLICY_URL) }
+    }
+
+    private fun openInternet(url: String){
+        val webpage = Uri.parse(url)
+        val intent = Intent(Intent.ACTION_VIEW, webpage)
+        if(intent.resolveActivity(requireActivity().packageManager) != null){
+            startActivity(intent)
+        }
+    }
+
+    private fun onCheckChanged(checkBox: CompoundButton) {
+        with(binding) {
+            when (checkBox.id) {
+                cbTosAll.id -> {
+                    if (cbTosAll.isChecked) {
+                        cbTosAge.isChecked = true
+                        cbTosPrivacy.isChecked = true
+                        cbTosTos.isChecked = true
+                    }else {
+                        cbTosAge.isChecked = false
+                        cbTosPrivacy.isChecked = false
+                        cbTosTos.isChecked = false
+                    }
+                }
+                else -> {
+                    cbTosAll.isChecked = (cbTosAge.isChecked && cbTosTos.isChecked && cbTosPrivacy.isChecked)
+                }
+            }
+
+            btnNext.alpha = if (cbTosAll.isChecked) 1f else 0.6f
+        }
+    }
+
+    private fun registerUser(): Boolean{
+        Log.i(TAG, "회원가입 요청")
+
+        if(registerToken == null) return false
+
+        val flag: Boolean = runBlocking(Dispatchers.IO) {
+            val result = viewModel.getResult()
+            if(result != null){
+                when(val res = RegisterUserUseCase().registerUser(registerToken!!, result)){
+                    is Resource.Success -> {
+                        Log.i(TAG, "회원가입 성공")
+                        GlobalApplication.app.getUserDataStore().updatePreferencesAccessToken(res.data.accessToken)
+                        GlobalApplication.app.getUserDataStore().updatePreferencesRefreshToken(res.data.refreshToken)
+
+                        true
+                    }
+                    is Resource.Error -> {
+                        Log.e(TAG, "회원가입 실패: ${res.message}")
+                        false
+                    }
+                    is Resource.Loading -> {
+                        false
+                    }
+                }
+            } else {
+                false
+            }
+        }
+
+        return flag
+    }
+}
