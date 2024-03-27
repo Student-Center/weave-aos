bf957330044d793129bd0e4eaca40e1bf5daabd0
moondev03 <moondev03@gmail.com>
2024-03-27 15:09:51 +0900

    [FIX] 요청 - 남은 시간 DateTimeParseException 처리 추가

--- Weave/app/src/main/java/com/studentcenter/weave/presentation/util/TimeUtil.kt
+++ Weave/app/src/main/java/com/studentcenter/weave/presentation/util/TimeUtil.kt
@@ -10,24 +10,14 @@ import java.time.format.DateTimeParseException
 class TimeUtil {
     fun getRemainingTimeMessage(endTimeString: String): String {
         val currentTime = LocalDateTime.now(ZoneId.of("Asia/Seoul"))
-        var parsedDateTime: LocalDateTime? = null
+        val parsedDateTime = parseDateTime(endTimeString)
 
-        try {
-            parsedDateTime = LocalDateTime.parse(endTimeString, DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSSSS"))
-        } catch (e: DateTimeParseException) {
-            try {
-                parsedDateTime = LocalDateTime.parse(endTimeString, DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSS"))
-            } catch (e: DateTimeParseException) {
-                try {
-                    parsedDateTime = LocalDateTime.parse(endTimeString, DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'"))
-                } catch (e: DateTimeParseException){
-                    Log.i("TimeUtil", "날짜 형식이 유효하지 않습니다.")
-                }
-            }
+        if (parsedDateTime == null) {
+            Log.i("TimeUtil", "날짜 형식이 유효하지 않습니다.")
+            return ""
         }
 
         val remainingTime = Duration.between(currentTime, parsedDateTime)
-
         val hours = remainingTime.toHours()
         val minutes = remainingTime.toMinutes() % 60
 
@@ -36,5 +26,30 @@ class TimeUtil {
         } else {
             "${minutes}분 뒤에 사라져요!"
         }
+    }
+
+    private fun parseDateTime(endTimeString: String): LocalDateTime? {
+        val patterns = arrayOf(
+            "yyyy-MM-dd'T'HH:mm:ss.SSSSSS",
+            "yyyy-MM-dd'T'HH:mm:ss.SSSSS",
+            "yyyy-MM-dd'T'HH:mm:ss.SSSS",
+            "yyyy-MM-dd'T'HH:mm:ss.SSS",
+            "yyyy-MM-dd'T'HH:mm:ss.SS",
+            "yyyy-MM-dd'T'HH:mm:ss.S",
+            "yyyy-MM-dd'T'HH:mm:ss"
+        )
+
+        for (pattern in patterns) {
+            try {
+                return if (pattern.endsWith("Z")) {
+                    LocalDateTime.parse(endTimeString, DateTimeFormatter.ofPattern(pattern))
+                } else {
+                    LocalDateTime.parse(endTimeString.replace("Z", ""), DateTimeFormatter.ofPattern(pattern))
+                }
+            } catch (e: DateTimeParseException) {
+                Log.i("TIME_UTIL", "$pattern -> 실패")
+            }
+        }
+        return null
     }
 }
